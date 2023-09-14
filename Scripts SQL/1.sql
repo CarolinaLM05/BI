855316c84dfc44ef5a0e23d773b99a5316798a3c
Carolina Lechuga <carolinalechugamartinez05@gmail.com>
2023-09-14 14:54:00 -0600

    ETL: Tarea Ejecucion SQL

---
+++ Scripts SQL/1.sql
@@ -0,0 +1,15 @@
+select * from Customers
+
+Select CustomerId from Customers
+where Country='France' and ContactName='Maria Anders'
+
+update Customers
+set Country='France'
+where CustomerID='ALFKI'
+
+update Customers
+set Country='Guanchumpein'
+where CustomerID=?
+
+Select * from Customers
+where CustomerID='ALFKI'
---
+++ Scripts SQL/Actividad Evaluacion Continua 1. Ejercicio de Tarea de Ejecución SQL.sql
@@ -0,0 +1,17 @@
+select * from Orders
+
+SELECT COUNT(*) as numero_de_registros
+FROM Orders
+
+SELECT * FROM Products
+
+UPDATE Products
+SET UnitsInStock = ?
+WHERE ProductID = 11
+
+SELECT * FROM Products
+WHERE ProductID = 11
+
+
+
+
